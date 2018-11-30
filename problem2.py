25d1664b18aa68eaff8c90bf2e0685cc0b33c0c0
osvaldohg <osvaldohg@gmail.com>
2018-11-30 16:03:08 +0000

    added solution without using division operation

--- problem2.py
+++ problem2.py
@@ -15,6 +15,28 @@ def solution(numbers):
     
     return ans
 
+#not using division
+def improved_solution(numbers):
+    left=[1 for i in range(len(numbers))]
+    right=[1 for i in range(len(numbers))]
+    prod=[]
+
+    for i in range(1,len(numbers)):
+        left[i]=numbers[i-1]*left[i-1]
+        
+    
+    for i in range(len(numbers)-2,-1,-1):
+        right[i]=numbers[i+1]*right[i+1]
+
+    for i in range(0,len(left)):
+        prod.append(left[i]*right[i])
+
+    return prod
+
 print solution([1,2,3,4,5])
 print solution([3,2,1])
 print solution([10, 3, 5, 6, 2])
+
+print improved_solution([1,2,3,4,5])
+print improved_solution([3,2,1])
+print improved_solution([10, 3, 5, 6, 2])
