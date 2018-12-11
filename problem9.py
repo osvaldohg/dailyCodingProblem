d5b9dc365214479460d05f13a43b91e301aa9142
osvaldohg <osvaldohg@gmail.com>
2018-12-11 22:09:43 +0000

    solved problem 9 with a dp approach

--- problem9.py
+++ problem9.py
@@ -1,5 +1,20 @@
 #Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
-
 #For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
-
 #Follow-up: Can you do this in O(N) time and constant space?
+
+
+def solution(numbers):
+    exc=0
+    inc=numbers[0]
+
+    for i in range(1,len(numbers)):
+        temp=inc
+        inc=max(exc+numbers[i],inc)
+        exc=temp
+
+    return inc
+
+
+print solution([2,4,6,2,5])#13
+print solution([5,1,1,5])#10
+print solution([5,5,10,100,10,5])#110
